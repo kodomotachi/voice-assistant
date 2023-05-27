173450a49662bd209ba625f8ac533e6ec1f764e4
kodomotachi <kodomotachi2k4@gmail.com>
2023-05-27 13:29:24 +0700

    Code main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# declare the window 
+window = Tk()
+
+# set window title
+window.title("Jarvis Voice Assistant")
+
+# set window background color
+window.configure(bg = 'lightgray')
+window.mainloop()
