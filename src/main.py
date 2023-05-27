671ad28572f143e1950fe512192701b7220197d7
kodomotachi <kodomotachi2k4@gmail.com>
2023-05-27 13:35:58 +0700

    Edit something

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ window = Tk()
 # set window title
 window.title("Jarvis Voice Assistant")
 
+# set window size
+window.configure(width = 500, height = 300)
+
 # set window background color
 window.configure(bg = 'lightgray')
 window.mainloop()
